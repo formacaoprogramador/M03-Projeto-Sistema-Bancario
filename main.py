8b450738e01319f76a845b00649ff27765fd7915
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2023-09-20 12:48:45 -0300

    Versão 02
    Vamos reestruturar o nosso código. Adicionar mais duas operações e trocar a nossa condição do WHILE para algo mais simples, objetivo e funcional.

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
+contas = []
+
 print(">>> Bem vindo ao sistema Bancário <<<")
 
-programa_ativo = True
-
-while programa_ativo == True:
+while True:
   print("\n### Menu ###")
   print("0 - Sair")
+  print("1 - Criar uma nova conta")
+  print("2 - Remover uma conta")
   numero_operacao = input("Selecione a operação que deseja realizar:\n>>> ")
 
   if numero_operacao == "0":
     print("\nSistema encerrado.")
-    programa_ativo = False
+    break
 
+  # Operação 1 - Criar uma nova conta
+  elif numero_operacao == "1":
+    verificador_conta_existe = False
+    dados_nova_conta = {}
+    dados_nova_conta['numero_conta'] = input("Digite o número da nova conta:\n>>> ")
+
+    for conta_em_consulta in contas:
+      if conta_em_consulta['numero_conta'] == dados_nova_conta['numero_conta']:
+        verificador_conta_existe = True
+
+    if verificador_conta_existe == True:
+      print("\nO número da conta já existe. Tente novamente.")
+    else:
+      dados_nova_conta['saldo_conta'] = float(input("Digite o saldo da nova conta:\n>>> "))
+      contas.append(dados_nova_conta)
+      print("\nOperacao efetuada com sucesso.")
+
+  # Operação 2 - Remover uma conta
+  elif numero_operacao == "2":
+    numero_conta = input("Digite o número da conta para remover:\n>>> ")
+    conta_encontrada = False
+    for conta_em_consulta in contas:
+      if conta_em_consulta['numero_conta'] == numero_conta:
+        conta_encontrada = True
+        contas.remove(conta_em_consulta)
+        print("\nOperação efetuada com sucesso.")
+    if not conta_encontrada:
+      print("\nO número da conta não existe. Tente novamente.")
+
+  # Operação inválida
   else:
     print("\nOperação invalida.")
