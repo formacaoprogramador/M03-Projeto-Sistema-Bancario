1f7446a9ae6e19e0062f181a166105e25e8848b5
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2023-09-20 12:49:11 -0300

    Versão 03
    Vamos reestruturar (refatorar) o nosso código e passar a usar funções.

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 contas = []
 
+def criar_conta():
+  verificador_conta_existe = False
+  dados_nova_conta = {}
+  dados_nova_conta['numero_conta'] = input('Digite o número da nova conta:\n>>> ')
+
+  for conta_em_consulta in contas:
+    if conta_em_consulta['numero_conta'] == dados_nova_conta['numero_conta']:
+      verificador_conta_existe = True
+      break
+
+  if verificador_conta_existe == True:
+    print('\nO número da conta já existe. Tente novamente.')
+  else:
+    dados_nova_conta['saldo_conta'] = float(input('Digite o saldo da nova conta:\n>>> '))
+    contas.append(dados_nova_conta)
+    print('\nOperação efetuada com sucesso.')
+
+def remover_conta():
+  numero_conta = input('Digite o número da conta para remover:\n>>> ')
+  conta_encontrada = False
+  for conta_em_consulta in contas:
+    if conta_em_consulta['numero_conta'] == numero_conta:
+      conta_encontrada = True
+      contas.remove(conta_em_consulta)
+      print('\nOperação efetuada com sucesso.')
+      break
+  if not conta_encontrada:
+    print('\nO número da conta não existe. Tente novamente.')
+
 print(">>> Bem vindo ao sistema Bancário <<<")
 
 while True:
@@ -15,32 +44,11 @@ while True:
 
   # Operação 1 - Criar uma nova conta
   elif numero_operacao == "1":
-    verificador_conta_existe = False
-    dados_nova_conta = {}
-    dados_nova_conta['numero_conta'] = input("Digite o número da nova conta:\n>>> ")
-
-    for conta_em_consulta in contas:
-      if conta_em_consulta['numero_conta'] == dados_nova_conta['numero_conta']:
-        verificador_conta_existe = True
-
-    if verificador_conta_existe == True:
-      print("\nO número da conta já existe. Tente novamente.")
-    else:
-      dados_nova_conta['saldo_conta'] = float(input("Digite o saldo da nova conta:\n>>> "))
-      contas.append(dados_nova_conta)
-      print("\nOperacao efetuada com sucesso.")
+    criar_conta()
 
   # Operação 2 - Remover uma conta
   elif numero_operacao == "2":
-    numero_conta = input("Digite o número da conta para remover:\n>>> ")
-    conta_encontrada = False
-    for conta_em_consulta in contas:
-      if conta_em_consulta['numero_conta'] == numero_conta:
-        conta_encontrada = True
-        contas.remove(conta_em_consulta)
-        print("\nOperação efetuada com sucesso.")
-    if not conta_encontrada:
-      print("\nO número da conta não existe. Tente novamente.")
+    remover_conta()
 
   # Operação inválida
   else:
