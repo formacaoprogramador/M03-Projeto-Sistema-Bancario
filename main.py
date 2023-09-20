b3d4e3812afcb889d38b67c4ae479ddf75a45eb7
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2023-09-20 12:50:45 -0300

    Versão 06
    Vamos criar a operação de Transferir Saldo entre Contas.

--- main.py
+++ main.py
@@ -44,34 +44,73 @@ def listar_contas():
       index += 1
 
 def adicionar_saldo_em_conta():
-  numero_conta = input("Digite o número da conta:\n>>> ")
+  numero_conta = input('Digite o número da conta:\n>>> ')
   for conta_em_consulta in contas:
     if conta_em_consulta['numero_conta'] == numero_conta:
-      valor_credito = float(input("Digite o valor do crédito:\n>>> R$ "))
+      valor_credito = float(input('Digite o valor do crédito:\n>>> R$ '))
       if valor_credito < 0:
-        print("\nValores negativos não são aceitos. Tente novamente.")
+        print('\nValores negativos não são aceitos. Tente novamente.')
         return
       else:
         conta_em_consulta['saldo_conta'] += valor_credito
-        print("\nOperação efetuada com sucesso.")
+        print('\nOperação efetuada com sucesso.')
         return
-  print("\nO número da conta não foi encontrado. Tente novamente.")
-
+  print('\nO número da conta não foi encontrado. Tente novamente.')
 
 def remover_saldo_em_conta():
-  numero_conta = input("Digite o número da conta:\n>>> ")
+  numero_conta = input('Digite o número da conta:\n>>> ')
   for conta_em_consulta in contas:
     if conta_em_consulta['numero_conta'] == numero_conta:
-      valor_debito = float(
-      input("Digite o valor do débito:\n>>> R$ "))
+      valor_debito = float(input('Digite o valor do débito:\n>>> R$ '))
       if valor_debito < 0:
-        print("\nValores negativos não são aceitos. Tente novamente.")
+        print('\nValores negativos não são aceitos. Tente novamente.')
         return
       else:
         conta_em_consulta['saldo_conta'] -= valor_debito
-        print("\nOperação efetuada com sucesso.")
+        print('\nOperação efetuada com sucesso.')
         return
-  print("\nO número da conta não foi encontrado. Tente novamente.")
+  print('\nO número da conta não foi encontrado. Tente novamente.')
+
+def transferir_saldo_entre_contas():
+  conta_partida = input('Digite o número da sua conta:\n>>> ')
+  verificador_etapa = 0
+  # Procura a primeira conta
+  for conta_em_consulta in contas:
+    if conta_em_consulta['numero_conta'] == conta_partida:
+      verificador_etapa = 1
+      conta_destino = input('Digite o número da conta destino:\n>>> ')
+      # Verifica se é uma transferencia para a mesma conta
+      if conta_destino != conta_partida:
+        # Procura a segunda conta
+        for conta_destino_em_consulta in contas:
+          if conta_destino_em_consulta['numero_conta'] == conta_destino:
+            verificador_etapa = 3
+            valor_transferencia = float(input('Digite o valor da transferência:\n>>> R$ '))
+            # Verifica se o valor é negativo
+            if valor_transferencia < 0:
+              print('\nNão é possível transferir um valor negativo.')
+              break
+            # Verifica se o saldo é suficiente
+            elif valor_transferencia > conta_em_consulta['saldo_conta']:
+              print('\nNão é possível transferir um valor maior que o seu saldo.')
+              break
+            else:
+              conta_em_consulta['saldo_conta'] -= valor_transferencia
+              conta_destino_em_consulta['saldo_conta'] += valor_transferencia
+              print('\nOperação efetuada com sucesso.')
+              break
+
+        print()
+      else:
+        verificador_etapa = 2
+        break
+
+  if verificador_etapa == 0:
+    print('\nA sua conta não foi encontrada. Tente novamente.')
+  elif verificador_etapa == 1:
+    print('\nA conta de destino não foi encontrada. Tente novamente.')
+  elif verificador_etapa == 2:
+    print('\nNão é possível transferir dinheiro para a mesma conta.')
 
 print(">>> Bem vindo ao sistema Bancário <<<")
 
@@ -83,6 +122,7 @@ while True:
   print("3 - Listar todas as contas")
   print("4 - Adicionar saldo (creditar)")
   print("5 - Remover saldo (debitar)")
+  print("6 - Transferir saldo entre contas")
   numero_operacao = input("Selecione a operação que deseja realizar:\n>>> ")
 
   if numero_operacao == "0":
@@ -108,6 +148,10 @@ while True:
   # Operação 5 - Débito
   elif numero_operacao == "5":
     remover_saldo_em_conta()
+
+  # Operação 6 - Transferir entre contas
+  elif numero_operacao == "6":
+    transferir_saldo_entre_contas()
 
   # Operação inválida
   else:
